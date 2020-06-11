1a9d3c6bdeed593b59c98731a48c7ab5122c08ef
Keerati <algobaleno.un@gmail.com>
2020-06-12 05:02:20 +0700

    add metadata facebook twitter

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,20 @@ export default function App({ Component, pageProps }) {
 
                 <meta content="" name="description" />
                 <meta content="" name="keywords" />
+
+                <meta property="og:url" content="https://raiteke.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="RAITEKE - KEERATI PORTFOLIO" />
+                <meta property="og:description" content="All the time traveling in the world can't make someone love you." />
+                <meta property="og:image" content="https://raiteke.com/img/bg.jpg" />
+
+                <meta content='summary_large_image' name='twitter:card' />
+                <meta content='@Algobaleno' name='twitter:site' />
+                <meta content='@Algobaleno' name='twitter:creator' />
+                <meta content="RAITEKE - KEERATI PORTFOLIO" name='twitter:title' />
+                <meta content="All the time traveling in the world can't make someone love you." name='twitter:description' />
+                <meta content="https://raiteke.com/img/bg.jpg" name='twitter:image' />
+
                 {/* Google Fonts */}
                 <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css" />
