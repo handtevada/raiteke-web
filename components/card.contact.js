d7a84e3709160c78db40052c44d723095348b271
Keerati <algobaleno.un@gmail.com>
2020-12-14 17:50:51 +0700

    Fixed contact card hover

--- components/card.contact.js
+++ components/card.contact.js
@@ -5,16 +5,14 @@ export default function CardContact(props) {
     <>
       <a className='card-contact' href={props.link} target='_blank'>
         <div className='card'>
-          <div className='card-content'>
-            <div className='content'>
-              <div className='columns'>
-                <div className='column is-one-fifth align-center card-contact-icon'>
-                  <img src={props.icon} alt={props.title} />
-                </div>
-                <div className='column'>
-                  <h1>{props.title}</h1>
-                  <p>{props.content}</p>
-                </div>
+          <div className='content'>
+            <div className='columns'>
+              <div className='column is-one-fifth align-center card-contact-icon'>
+                <img src={props.icon} alt={props.title} />
+              </div>
+              <div className='column'>
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
               </div>
             </div>
           </div>
@@ -24,12 +22,13 @@ export default function CardContact(props) {
         .card {
           border-radius: 10px;
           min-height: 150px;
+          padding: 1.5rem;
         }
         .card-contact-icon {
           color: #4a4a4a;
           max-width: 100px;
         }
-        .card-content:hover {
+        .card:hover {
           background-color: #f1f1f1;
           border-radius: 10px;
         }
