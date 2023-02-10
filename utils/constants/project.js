46b67301e196fdcd951542b05eadbb5c7f2d7458
keerati yuonghirun <6884279-directorys@users.noreply.gitlab.com>
2023-02-10 16:39:27 +0700

    feat(Projects): Add new project, SET chrome extension

--- utils/constants/project.js
+++ utils/constants/project.js
@@ -5,6 +5,7 @@ import {
   AiOutlineFieldTime,
 } from 'react-icons/ai';
 import { MdChildCare } from 'react-icons/md';
+import { RiStockLine } from 'react-icons/ri';
 
 const projects = [
   {
@@ -14,6 +15,13 @@ const projects = [
     blog: 'https://medium.com/@directorys/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-portfolio-web-app-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%A3%E0%B9%8B%E0%B9%86-%E0%B8%AA%E0%B9%84%E0%B8%95%E0%B8%A5%E0%B9%8C%E0%B9%82%E0%B8%9B%E0%B8%A3%E0%B9%81%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B9%80%E0%B8%A1%E0%B8%AD%E0%B8%A3%E0%B9%8C-8c82175a71bb',
     icon: <AiOutlineProfile />,
     content: `My portfolio, Present by terminal command like a developer style.`,
+  },
+  {
+    title: 'SET Chrome Extension',
+    github: 'https://github.com/handtevada/set-summary-extension',
+    blog: 'https://medium.com/@directorys/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87-chrome-extension-%E0%B8%94%E0%B8%B9%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%AB%E0%B8%B8%E0%B9%89%E0%B8%99%E0%B9%83%E0%B8%99%E0%B9%80%E0%B8%A7%E0%B9%87%E0%B8%9A-set-or-th-2aa9f384b6f7',
+    icon: <RiStockLine />,
+    content: `Chrome extension, Summary data on many tabs of www.set.or.th in one page`,
   },
   {
     title: 'History Timelines',
